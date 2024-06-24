3217f7b9b2400b8a519caf12c54d0f5092d3753d
ppchavan552002 <ppchavan552002@gmail.com>
2024-06-24 18:55:14 +0530

    changes

--- FileIOJava/src/FileIOJavaDemo/ConsoleJava.java
+++ FileIOJava/src/FileIOJavaDemo/ConsoleJava.java
@@ -7,9 +7,23 @@ public class ConsoleJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Console c=System.console();
-		System.out.println("Enter your name");
-		String n=c.readLine();
-		System.out.println("welcome "+n);
+		if (c != null) {
+            System.out.println("Enter your name:");
+            String n = c.readLine();
+            System.out.println("Welcome " + n);
+        } else {
+            System.out.println("No console available. Please run this program from a console.");
+        }
 		}
 
 }
+/*
+	C:\Users\Victory>cd C:\Users\Victory\OneDrive\Desktop\java_prog_chapter_wise\FileIOJava\src
+
+	C:\Users\Victory\OneDrive\Desktop\java_prog_chapter_wise\FileIOJava\src>javac FileIOJavaDemo\ConsoleJava.java
+	
+	C:\Users\Victory\OneDrive\Desktop\java_prog_chapter_wise\FileIOJava\src>java FileIOJavaDemo.ConsoleJava
+	Enter your name:
+	prathaemsh
+	Welcome prathaemsh
+ */
